82de6bd5cb0fdc9644e2731d77ec059070a23bf7
Haris Wahidin <hrswhdn@gmail.com>
2025-08-19 20:47:00 +0700

    add alien_invasion

---
+++ alien_invasion/settings.py
@@ -0,0 +1,12 @@
+class Settings:
+  def __init__(self):
+    self.screen_width = 1200
+    self.screen_height = 800
+    self.bg_color = (0,148,255)
+    self.ship_speed = 10
+    self.ship_speed_diagonal = 7.5
+    self.bullet_speed = 2.0
+    self.bullet_width = 3
+    self.bullet_height = 15
+    self.bullet_color = (189,62,62)
+    self.bullets_allowed = 30
